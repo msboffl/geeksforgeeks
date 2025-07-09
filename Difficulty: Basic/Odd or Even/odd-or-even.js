85a9cb2979ea27967d5d452a5e682e12ab1d8932
Mareedu Saibabu <53231064+msboffl@users.noreply.github.com>
2025-07-10 00:43:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Odd or Even/odd-or-even.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number} n
+ * @return {bool}
+ */
+
+class Solution {
+    isEven(n) {
+        // code here
+        if(n % 2 === 0) {
+            return true;
+        } 
+        retur
+    }
+}
