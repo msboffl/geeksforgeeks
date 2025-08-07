6d589f5b998a3bbb0ca45a3a10569ba5b5387681
Mareedu Saibabu <53231064+msboffl@users.noreply.github.com>
2025-08-07 20:10:02 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/The dice problem/the-dice-problem.js
@@ -0,0 +1,12 @@
+// User function Template for javascript
+
+/**
+ * @param {number} N
+ * @return {number}
+ */
+class Solution {
+    oppositeFaceOfDice(n) {
+        // code here
+        return 7 - n;
+    }
+}
