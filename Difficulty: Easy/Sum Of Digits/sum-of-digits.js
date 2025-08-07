ae433bb17abd49822b11d428cff694a1f8ba6e9e
Mareedu Saibabu <53231064+msboffl@users.noreply.github.com>
2025-08-07 20:32:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sum Of Digits/sum-of-digits.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+
+class Solution {
+    sumOfDigits(n) {
+        // code here
+        let sum = 0;
+        while(n > 0) {
+            let rem = n % 10;
+            sum += rem;
+            n = Math.floor(n / 10);
+        }
+        
+        return sum;
+    }
+}
