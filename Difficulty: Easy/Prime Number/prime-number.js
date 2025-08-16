da08641b96499c71af37bf6f8fd5ba50b4478e7d
Mareedu Saibabu <53231064+msboffl@users.noreply.github.com>
2025-08-16 18:39:31 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Prime Number/prime-number.js
+++ Difficulty: Easy/Prime Number/prime-number.js
@@ -8,7 +8,7 @@ class Solution {
         if(n <= 1) return false;
         
         for(let i = 2; i * i <= n; i++) {
-            if(n % i === 0) return false
+            if(n % i === 0) return false;
         }
         
         return true;
