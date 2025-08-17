ec88a414dd88a9d9cfe0dd2d6cbd3c0427aa5da2
Mareedu Saibabu <53231064+msboffl@users.noreply.github.com>
2025-08-17 11:28:45 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Square Root/square-root.js
+++ Difficulty: Easy/Square Root/square-root.js
@@ -6,6 +6,21 @@
 class Solution {
     floorSqrt(n) {
         // code here
-        return Math.floor(Math.sqrt(n))
+        let ans = 1;
+        let left = 0;
+        let right = n;
+        
+        while(left <= right) {
+            let mid = left + Math.floor((right - left) / 2)
+            
+            if(mid * mid <= n) {
+                ans = mid;
+                left = mid + 1
+            } else {
+                right = mid - 1
+            }
+        }
+        
+        return ans;
     }
 }
