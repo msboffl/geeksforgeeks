f0db3f75fc1ac7678dd1758be018249ff9015434
Mareedu Saibabu <53231064+msboffl@users.noreply.github.com>
2025-08-27 11:26:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Sum of Natural Numbers/sum-of-natural-numbers.js
+++ Difficulty: Basic/Sum of Natural Numbers/sum-of-natural-numbers.js
@@ -6,15 +6,10 @@ class Solution {
     findSum(n) {
         // code here
         let sum = 0;
-        function sumOfNaturals(i) {
-            if(i > n) return;
+        for(let i = 0; i <= n; i++) {
             sum += i;
-            
-            sumOfNaturals(i+1);
         }
         
-        let i = 1;
-        sumOfNaturals(1)
         return sum;
     }
 }
