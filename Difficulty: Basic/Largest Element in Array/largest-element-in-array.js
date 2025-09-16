6d57a3100eb3dc04690eb9909cfbad696c8a3f36
Mareedu Saibabu <53231064+msboffl@users.noreply.github.com>
2025-09-16 10:34:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Largest Element in Array/largest-element-in-array.js
@@ -0,0 +1,18 @@
+
+class Solution {
+    /**
+    * @param number[] arr
+
+    * @returns number
+    */
+    largest(arr) {
+        // code here
+        let max = arr[0];
+        for(let i = 1; i < arr.length; i++) {
+            if(arr[i] > max) max = arr[i];
+            
+        }
+        
+        return max;
+    }
+}
