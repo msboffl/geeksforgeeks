b15bb4684340e7130b62c7f493535c785b214781
Mareedu Saibabu <53231064+msboffl@users.noreply.github.com>
2025-10-22 17:54:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Sum of Natural Numbers/sum-of-natural-numbers.js
+++ Difficulty: Basic/Sum of Natural Numbers/sum-of-natural-numbers.js
@@ -1,10 +1,16 @@
+/**
+ * @param {number} n
+ * @returns {number}
+ */
 class Solution {
-    /**
-     * @param number n
-     * @returns number
-     */
     findSum(n) {
         // code here
-        return n * (n+1)/2
+        let sum = 0;
+        
+        for(let i = 1; i <= n; i++) {
+            sum += i
+        }
+        
+        return sum;
     }
 }
