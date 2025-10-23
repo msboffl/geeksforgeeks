ffec9c921151adeb3612e02cee7129498d646c0c
Mareedu Saibabu <53231064+msboffl@users.noreply.github.com>
2025-10-23 19:45:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sum of First N Natural Numbers/sum-of-first-n-natural-numbers.js
@@ -0,0 +1,13 @@
+// User function Template for javascript
+/**
+ * @param {number} n
+ * @returns {number}
+ */
+
+class Solution {
+    // Function to calculate the sum of the first n natural numbers.
+    sumOfNaturals(n) {
+        // code here
+        return (n * (n + 1)) / 2
+    }
+}
