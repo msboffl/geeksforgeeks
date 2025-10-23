1151f6c000e992923ea37bc2a502f4028a5d3d27
Mareedu Saibabu <53231064+msboffl@users.noreply.github.com>
2025-10-23 19:37:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Squares in N*N Chessboard/squares-in-nn-chessboard.js
@@ -0,0 +1,13 @@
+// User function Template for javascript
+
+/**
+ * @param {number} n
+ * @returns {number}
+ */
+
+class Solution {
+    squaresInChessBoard(n) {
+        // code here
+        return (n * (n+1) * (2*n+1)) / 6 ;
+    }
+}
