538ead82462421c4e3325686ae715c038184badb
Mareedu Saibabu <53231064+msboffl@users.noreply.github.com>
2025-10-23 19:31:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Find N-th term in the series/find-nth-term-in-the-series.js
@@ -0,0 +1,13 @@
+// User function Template for javascript
+
+/**
+ * @param {number} n
+ * @returns {number}
+ */
+
+class Solution {
+    nthOfSeries(n) {
+        // code here
+        return (8 * n * n) + 1
+    }
+}
