3af57354306a52e1f9e4150d84b2787391630a9a
Mareedu Saibabu <53231064+msboffl@users.noreply.github.com>
2025-10-23 19:39:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Area of a Circle/area-of-a-circle.java
@@ -0,0 +1,8 @@
+// User function Template for Java
+class Solution {
+    // Method to calculate the area (assuming it's a circle with the given radius)
+    public double calculateArea(double r) {
+        // code here
+        return Math.PI * r * r;
+    }
+}
