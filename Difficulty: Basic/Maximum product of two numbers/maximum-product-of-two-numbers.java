a36a027dcf0c0e8da3ff173704a9dc490861460c
Mareedu Saibabu <53231064+msboffl@users.noreply.github.com>
2025-12-24 08:45:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Maximum product of two numbers/maximum-product-of-two-numbers.java
@@ -0,0 +1,9 @@
+
+class Solution {
+    public static int maxProduct(int[] arr) {
+        // code here
+        Arrays.sort(arr);
+        
+        return arr[arr.length-1] * arr[arr.length-2];
+    }
+}
